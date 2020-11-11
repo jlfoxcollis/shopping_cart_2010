d6c90d787e424b6ecd7dd05c1e1b54df13eb7087
James Fox-Collis <jlfoxcollis@gmail.com>
2020-11-11 09:32:50 -0700

    overstocked items compelete

--- lib/market.rb
+++ lib/market.rb
@@ -20,4 +20,22 @@ class Market
     end
     inventory
   end
+
+  def vendors_that_sell(item)
+    vendors.select do |vendor|
+      vendor.inventory.include?(item) && vendor.inventory[item] != 0
+    end
+  end
+
+  def overstocked_items
+    overstock = []
+    vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        if vendors_that_sell(item).count > 1 && quantity > 50
+          overstock << item
+        end
+      end
+    end
+    overstock
+  end
 end
